af7f364a7fc1ac12080e9a4193e3a3f4949f1e49
Denis Obydennykh <denis.obydennykh@team.wrike.com>
2016-07-19 16:56:55 +0300

    minor - analyzer fixes

--- test/TemplateClient_test.dart
+++ test/TemplateClient_test.dart
@@ -1,19 +1,19 @@
 // Copyright (c) 2016, Denis Obydennykh. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:TemplateClient/TemplateClient.dart';
+import 'package:wrakaton_template_client/TemplateClient.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    Awesome awesome;
+    //Awesome awesome;
 
     setUp(() {
-      awesome = new Awesome();
+      //awesome = new Awesome();
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      //expect(awesome.isAwesome, isTrue);
     });
   });
 }
